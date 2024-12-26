9fa7093a17ae26b2468f0e22d2551fa8fc156bdc
Thomas (DJ1TJOO) <t.ferb1@gmail.com>
2024-12-26 21:16:50 +0100

    Reworked open gui command to support multiple guis

--- src/main/java/me/dj1tjoo/customgui/CustomGUIPlugin.java
+++ src/main/java/me/dj1tjoo/customgui/CustomGUIPlugin.java
@@ -1,6 +1,7 @@
 package me.dj1tjoo.customgui;
 
 import me.dj1tjoo.customgui.commands.OpenGUICommand;
+import org.bukkit.command.PluginCommand;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class CustomGUIPlugin extends JavaPlugin {
@@ -9,7 +10,10 @@ public final class CustomGUIPlugin extends JavaPlugin {
         // Plugin startup logic
 //        getServer().getPluginManager().registerEvents(new PlayerJoinListener(this), this);
 
-        getCommand(OpenGUICommand.COMMAND).setExecutor(new OpenGUICommand(this));
+        PluginCommand
+            openGUI = getCommand(OpenGUICommand.COMMAND);
+        openGUI.setTabCompleter(new OpenGUICommand(this));
+        openGUI.setExecutor(new OpenGUICommand(this));
     }
 
     @Override
--- src/main/java/me/dj1tjoo/customgui/commands/OpenGUICommand.java
+++ src/main/java/me/dj1tjoo/customgui/commands/OpenGUICommand.java
@@ -1,46 +1,92 @@
 package me.dj1tjoo.customgui.commands;
 
 import me.dj1tjoo.customgui.CustomGUIPlugin;
-import me.dj1tjoo.customgui.guis.BasicGUI;
+import me.dj1tjoo.customgui.guis.CleanVentGUI;
 import me.dj1tjoo.customgui.guis.FirstGUI;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.command.TabCompleter;
 import org.bukkit.entity.Player;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.UUID;
 
-public class OpenGUICommand implements CommandExecutor {
+public class OpenGUICommand implements CommandExecutor, TabCompleter {
 
     public static String COMMAND = "open-gui";
+    private static List<String> GUIS = List.of("first", "clean_vent");
     private final CustomGUIPlugin plugin;
 
-    private final Map<UUID, BasicGUI> guis;
+    private final Map<UUID, FirstGUI> firstGUIMap;
+    private final Map<UUID, CleanVentGUI> cleanVentGUIMap;
 
     public OpenGUICommand(CustomGUIPlugin plugin) {
         this.plugin = plugin;
-        this.guis = new HashMap<>();
+        this.firstGUIMap = new HashMap<>();
+        this.cleanVentGUIMap = new HashMap<>();
     }
 
     @Override
     public boolean onCommand(@NotNull CommandSender commandSender, @NotNull Command command,
-                             @NotNull String s, @NotNull String[] strings) {
-        if (!(commandSender instanceof Player player)) {
+                             @NotNull String alias, @NotNull String[] args) {
+        if (!(commandSender instanceof Player player) || !command.getName().equals(COMMAND)) {
+            return true;
+        }
+
+        if (args.length == 0) {
+            return false;
+        }
+
+        if (!GUIS.contains(args[0])) {
+            commandSender.sendMessage("GUI not found");
             return true;
         }
 
         UUID uuid = player.getUniqueId();
-        if (!guis.containsKey(uuid)) {
-            guis.put(uuid, new FirstGUI(plugin));
+
+        switch (args[0]) {
+            case "first":
+                if (!firstGUIMap.containsKey(uuid)) {
+                    firstGUIMap.put(uuid, new FirstGUI(plugin));
+                }
+
+                firstGUIMap.get(uuid).open(player);
+                break;
+
+            case "clean_vent":
+                if (!cleanVentGUIMap.containsKey(uuid)) {
+                    cleanVentGUIMap.put(uuid, new CleanVentGUI(plugin));
+                }
+
+                cleanVentGUIMap.get(uuid).open(player);
         }
 
-        guis.get(uuid).open(player);
-//        new FirstGUI(plugin).open(player::openInventory);
         // /give DJ1TJOO minecraft:oak_button[minecraft:item_model='custom_gui:empty',minecraft:hide_tooltip={},minecraft:item_name='Opslaan',minecraft:max_stack_size=1]
         // /give DJ1TJOO minecraft:oak_button[minecraft:item_model='custom_gui:opslaan',minecraft:hide_tooltip={},minecraft:item_name='Opslaan',minecraft:max_stack_size=1]
         return true;
+    }
+
+    @Override
+    public @Nullable List<String> onTabComplete(@NotNull CommandSender commandSender,
+                                                @NotNull Command command, @NotNull String alias,
+                                                @NotNull String[] args) {
+        if (!(commandSender instanceof Player player) || !command.getName().equals(COMMAND)) {
+            return null;
+        }
+
+        if (args.length > 1) {
+            return null;
+        }
+
+        if (args.length == 1) {
+            return GUIS.stream().filter(string -> string.startsWith(args[0])).toList();
+        }
+
+        return GUIS;
     }
 }
